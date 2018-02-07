b26c45201dca8ef42c4af7249062d687305f850c
Matthew Rocklin <mrocklin@gmail.com>
2018-02-07 13:08:08 -0500

    remove install-circle

--- ci/install-circle.sh
+++
@@ -1,8 +0,0 @@
-set -xe
-
-conda update -q conda
-conda env create -f ci/environment-${PYTHON}.yml --name=${ENV_NAME}
-conda env list
-source activate ${ENV_NAME}
-pip install --no-deps --quiet -e .
-conda list ${ENV_NAME}
