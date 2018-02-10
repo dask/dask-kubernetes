91f27855a5e9c3e5b954f2de9091117ed05f95c0
Matthew Rocklin <mrocklin@gmail.com>
2018-02-10 17:04:17 -0500

    add config docstring

--- daskernetes/config.py
+++ daskernetes/config.py
@@ -1,3 +1,17 @@
+"""
+We capture all environment variables named
+
+    DASKERNETES_FOO_BAR=value
+
+And turn them into key-value pairs in a global ``config`` dictionary.
+We change the keys to remove ``DASKERNETES_``, replace underscores with
+hyphens, and lower-case the entire thing.  This results in a Python dictionary
+like the following:
+
+    >>> config
+    {'foo-bar': 'value'}
+"""
+
 import os
 
 config = {'-'.join(k.split('_')[1:]).lower(): v
