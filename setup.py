428ff27d3d0c618cf0768a8e0ca81cbdeab9cd39
Matthew Rocklin <mrocklin@gmail.com>
2018-02-12 16:15:21 -0500

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='daskernetes',
-    version='0.1.0',
+    version='0.1.1',
     description='Native Kubernetes integration for Dask',
     url='https://github.com/dask/daskernetes',
     keywords='dask,kubernetes,distributed',
     license='BSD',
     packages=find_packages(),
-    long_description=(open('README.md').read() if exists('README.md') else ''),
+    long_description=(open('README.rst').read() if exists('README.md') else ''),
     zip_safe=False,
     install_requires=list(open('requirements.txt').read().strip().split('\n')),
 )
