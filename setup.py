f103a783bf934c5477e6c0f4a0d5d935f9292ec8
Matthew Rocklin <mrocklin@gmail.com>
2018-02-12 16:16:56 -0500

    bump version to 0.1.2
    Sigh, another typo in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='daskernetes',
-    version='0.1.1',
+    version='0.1.2',
     description='Native Kubernetes integration for Dask',
     url='https://github.com/dask/daskernetes',
     keywords='dask,kubernetes,distributed',
     license='BSD',
     packages=find_packages(),
-    long_description=(open('README.rst').read() if exists('README.md') else ''),
+    long_description=(open('README.rst').read() if exists('README.rst') else ''),
     zip_safe=False,
     install_requires=list(open('requirements.txt').read().strip().split('\n')),
 )
