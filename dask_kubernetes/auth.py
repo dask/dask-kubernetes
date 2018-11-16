de70bda4450910fafb3e9f35195747dc6e325587
Derek Ludwig <dludwig@continuum.io>
2018-11-16 07:53:46 -0800

    add tests for cluster authentication methods

---
+++ dask_kubernetes/auth.py
@@ -0,0 +1,157 @@
+"""
+Defines different methods to configure a connection to a Kubernetes cluster.
+"""
+import logging
+
+import kubernetes
+
+logger = logging.getLogger(__name__)
+
+
+class ClusterAuth(object):
+    """
+    An abstract base class for methods for configuring a connection to a
+    Kubernetes API server.
+
+    Examples
+    --------
+    >>> from dask_kubernetes import KubeConfig
+    >>> auth = KubeConfig(context='minikube')
+
+    >>> from dask_kubernetes import KubeAuth
+    >>> auth = KubeAuth(host='https://localhost', username='superuser', password='pass')
+
+    """
+
+    def load(self):
+        """
+        Load Kubernetes configuration and set as default
+
+        Raises
+        ------
+
+        kubernetes.client.KubeConfigException
+        """
+        raise NotImplementedError()
+
+
+    @staticmethod
+    def load_first(auth=None):
+        """
+        Load the first valid configuration in the list *auth*. A single
+        configuration method can be passed.
+
+        Parameters
+        ----------
+
+        auth: List[ClusterAuth] (optional)
+            Configuration methods to attempt in order.  Defaults to
+            ``[InCluster(), KubeConfig()]``.
+
+        """
+        if auth is None:
+            auth = ClusterAuth.DEFAULT
+        elif isinstance(auth, ClusterAuth):
+            auth = [auth]
+        elif isinstance(auth, list):
+            if not auth:
+                raise kubernetes.config.ConfigException('No authorization methods were provided')
+        else:
+            msg = ("Invalid authorization method provided. See ClusterAuth "
+                   "docstring for ways to specify authentication methods")
+            raise ValueError(msg)
+
+        auth_exc = None
+        for auth_instance in auth:
+            try:
+                auth_instance.load()
+            except kubernetes.config.ConfigException as exc:
+                logger.debug('Failed to load configuration with %s method: %s', auth_instance.__class__, exc)
+                auth_exc = exc
+            else:
+                break
+        else:
+            raise auth_exc
+
+
+class InCluster(ClusterAuth):
+    """Configure the Kubernetes connection from a container's environment.
+
+    This authentication method is intended for use when the client is running
+    in a container started by Kubernetes with an authorized service account.
+    This loads the mounted service account token and discovers the Kubernetes
+    API via Kubernetes service discovery.
+    """
+    def load(self):
+        kubernetes.config.load_incluster_config()
+
+
+class KubeConfig(ClusterAuth):
+    """Configure the Kubernetes connection from a kubeconfig file.
+
+    Parameters
+    ----------
+    config_file: str (optional)
+        The path of the kubeconfig file to load.  Defaults to the value of the
+        ``KUBECONFIG`` environment variable, or the string ``~/.kube/config``.
+    context: str (optional)
+        The kubeconfig context to use.  Defaults to the value of ``current-context``
+        in the configuration file.
+    persist_config: bool (optional)
+        Whether changes to the configuration will be saved back to disk (e.g.
+        GCP token refresh).  Defaults to ``True``.
+
+    """
+    def __init__(self, config_file=None, context=None, persist_config=True):
+        self.config_file = config_file
+        self.context = context
+        self.persist_config = persist_config
+
+    def load(self):
+        kubernetes.config.load_kube_config(self.config_file, self.context,
+                                           None, self.persist_config)
+
+
+class KubeAuth(ClusterAuth):
+    """Configure the Kubernetes connection explicitly.
+
+    Parameters
+    ----------
+    host: str
+        The base URL of the Kubernetes host to connect
+    username: str (optional)
+        Username for HTTP basic authentication
+    password: str (optional)
+        Password for HTTP basic authentication
+    debug: bool (optional)
+        Debug switch
+    verify_ssl: bool (optional)
+        Set this to false to skip verifying SSL certificate when calling API
+        from https server.  Defaults to ``True``.
+    ssl_ca_cert: str (optional)
+        Set this to customize the certificate file to verify the peer.
+    cert_file: str (optional)
+        Client certificate file
+    key_file: str (optional)
+        Client key file
+    assert_hostname: bool (optional)
+        Set this to True/False to enable/disable SSL hostname verification.
+        Defaults to True.
+    proxy: str (optional)
+        URL for a proxy to connect through
+    """
+    def __init__(self, host, **kwargs):
+        # We need to create a new configuration in this way, because if we just
+        # instantiate a new Configuration object we will get the default
+        # values.
+        config = type.__call__(kubernetes.client.Configuration)
+        config.host = host
+        for key, value in kwargs.items():
+            setattr(config, key, value)
+        self.config = config
+
+    def load(self):
+        kubernetes.client.Configuration.set_default(self.config)
+
+
+ClusterAuth.DEFAULT = [InCluster(), KubeConfig()]
--- dask_kubernetes/core.py
+++ dask_kubernetes/core.py
@@ -20,92 +20,9 @@ import kubernetes
 from tornado import gen
 
 from .objects import make_pod_from_dict, clean_pod_template
+from .auth import ClusterAuth
 
 logger = logging.getLogger(__name__)
-
-
-class ClusterAuth(object):
-    def load(self):
-        raise NotImplementedError()
-
-
-class InCluster(ClusterAuth):
-    """
-    Gather configuration from the mounted service account token and discover
-    Kubernetes service via Kubernetes service discovery.
-    """
-    def load(self):
-        kubernetes.config.load_incluster_config()
-
-
-class KubeConfig(ClusterAuth):
-    """
-    Load configuration from a kubeconfig file.
-
-    Parameters
-    ----------
-    config_file: str (optional)
-        The path of the kubeconfig file to load.  Defaults to the value of the
-        ``KUBECONFIG`` environment variable, or the string ``~/.kube/config``.
-    context: str (optional)
-        The kubeconfig context to use.  Defaults to the value of ``current-context``
-        in the configuration file.
-    persist_config: bool (optional)
-        Whether changes to the configuration will be saved back to disk (e.g.
-        GCP token refresh).  Defaults to ``True``.
-    """
-    def __init__(self, config_file=None, context=None, persist_config=True):
-        self.config_file = config_file
-        self.context = context
-        self.persist_config = persist_config
-
-    def load(self):
-        kubernetes.config.load_kube_config(self.config_file, self.context,
-                                           None, self.persist_config)
-
-
-class KubeAuth(ClusterAuth):
-    """Authentication method to configuration the Kubernetes connection
-    directly.
-
-    Parameters
-    ----------
-    host: str
-        The base URL of the Kubernetes host to connect
-
-    Keyword Arguments
-    -----------------
-    username: str (optional)
-        Username for HTTP basic authentication
-    password: str (optional)
-        Password for HTTP basic authentication
-    debug: bool (optional)
-        Debug switch
-        # SSL/TLS verification
-    verify_ssl: bool (optional)
-        Set this to false to skip verifying SSL certificate when calling API
-        from https server.  Defaults to ``True``.
-    ssl_ca_cert: str (optional)
-        Set this to customize the certificate file to verify the peer.
-    cert_file: str (optional)
-        Client certificate file
-    key_file: str (optional)
-        Client key file
-    assert_hostname: bool (optional)
-        Set this to True/False to enable/disable SSL hostname verification.
-        Defaults to True.
-    proxy: str (optional)
-        URL for a proxy to connect through
-    """
-    def __init__(self, host, **kwargs):
-        config = kubernetes.client.Configuration()
-        config.host = host
-        for key, value in kwargs.items():
-            setattr(config, key, value)
-        self.config = config
-
-    def load(self):
-        kubernetes.config.Configuration.set_default(self.config)
 
 
 class KubeCluster(Cluster):
@@ -155,6 +72,9 @@ class KubeCluster(Cluster):
         Listen address for local scheduler.  Defaults to 0.0.0.0
     port: int
         Port of local scheduler
+    auth: List[ClusterAuth] (optional)
+        Configuration methods to attempt in order.  Defaults to
+        ``[InCluster(), KubeConfig()]``.
     **kwargs: dict
         Additional keyword arguments to pass to LocalCluster
 
@@ -230,7 +150,7 @@ class KubeCluster(Cluster):
             host=None,
             port=None,
             env=None,
-            auth=[InCluster(), KubeConfig()],
+            auth=ClusterAuth.DEFAULT,
             **kwargs
     ):
         name = name or dask.config.get('kubernetes.name')
@@ -263,20 +183,7 @@ class KubeCluster(Cluster):
                                     scheduler_port=port,
                                     n_workers=0, **kwargs)
 
-        if isinstance(auth, ClusterAuth):
-            auth = [auth]
-        auth_exc = ValueError('No authorization methods were provided')
-        for auth_instance in auth:
-            try:
-                auth_instance.load()
-            except kubernetes.config.ConfigException as exc:
-                logger.debug('Failed to load configuration with %s method: %s', auth_instance.__class__, exc)
-                auth_exc = exc
-            else:
-                break
-        else:
-            raise auth_exc
-
+        ClusterAuth.load_first(auth)
 
         self.core_api = kubernetes.client.CoreV1Api()
 
