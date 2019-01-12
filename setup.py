458da55c2e44a15d9147dfbb9c2a80bf67598bc7
Matthew Rocklin <mrocklin@gmail.com>
2019-01-11 16:33:50 -0800

    bump version to 0.7.0
    This commit is unique in that it supports Python 2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dask-kubernetes',
-    version='0.6.0',
+    version='0.7.0',
     description='Native Kubernetes integration for Dask',
     url='https://github.com/dask/dask-kubernetes',
     keywords='dask,kubernetes,distributed',
@@ -15,4 +15,5 @@ setup(
     long_description=(open('README.rst').read() if exists('README.rst') else ''),
     zip_safe=False,
     install_requires=list(open('requirements.txt').read().strip().split('\n')),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 )
