87f8378c399af77590eb5126f175bd63b60851cd
adam <athornton@gmail.com>
2019-04-24 14:05:48 -0700

    k8s API changed in 9.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dask-kubernetes',
-    version='0.7.0',
+    version='0.7.1',
     description='Native Kubernetes integration for Dask',
     url='https://github.com/dask/dask-kubernetes',
     keywords='dask,kubernetes,distributed',
