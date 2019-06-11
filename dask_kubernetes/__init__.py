d853598c5074df5d08dd219230d4c5cdf1d5504a
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-06-11 13:15:19 -0500

    fixup

--- dask_kubernetes/__init__.py
+++ dask_kubernetes/__init__.py
@@ -1,8 +1,7 @@
 from . import config
 from .auth import ClusterAuth, KubeAuth, KubeConfig, InCluster
 from .core import KubeCluster
-from .objects import (
-        make_pod_spec, make_pod_from_dict)
+from .objects import make_pod_spec, make_pod_from_dict
 
 __all__ = [KubeCluster]
 
