efdd8066d141ad62586d797448d160cb577192b0
James Lamb <jaylamb20@gmail.com>
2021-05-23 22:42:22 -0500

    fix __all__

--- dask_kubernetes/__init__.py
+++ dask_kubernetes/__init__.py
@@ -11,7 +11,7 @@ from .core import KubeCluster
 from .helm import HelmCluster
 from .objects import make_pod_spec, make_pod_from_dict, clean_pod_template
 
-__all__ = [KubeCluster, HelmCluster]
+__all__ = ["HelmCluster", "KubeCluster"]
 
 from ._version import get_versions
 
