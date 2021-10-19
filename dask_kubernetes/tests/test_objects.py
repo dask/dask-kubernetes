6c7f09875c439ad40353a49b859119d0277d5124
Ben Perry <bhperry94@gmail.com>
2021-10-19 04:44:42 -0500

    Set default container name when deserializing pod template (#354)
    * Set default container name when deserializing pod template
    
    * lint
    
    Co-authored-by: Jacob Tomlinson <jtomlinson@nvidia.com>

--- dask_kubernetes/tests/test_objects.py
+++ dask_kubernetes/tests/test_objects.py
@@ -117,3 +117,35 @@ def test_make_pod_from_dict():
     assert pod.spec.restart_policy == "Never"
     assert pod.spec.containers[0].security_context.privileged
     assert pod.spec.containers[0].security_context.capabilities.add == ["SYS_ADMIN"]
+
+
+def test_make_pod_from_dict_default_container_name():
+    d = {
+        "kind": "Pod",
+        "metadata": {"labels": {"app": "dask", "dask.org/component": "dask-worker"}},
+        "spec": {
+            "containers": [
+                {
+                    "args": [
+                        "dask-worker",
+                        "$(DASK_SCHEDULER_ADDRESS)",
+                        "--nthreads",
+                        "1",
+                    ],
+                    "image": "image-name",
+                    "securityContext": {
+                        "capabilities": {"add": ["SYS_ADMIN"]},
+                        "privileged": True,
+                    },
+                },
+                {"image": "image-name2", "name": "sidecar"},
+                {"image": "image-name3"},
+            ],
+            "restartPolicy": "Never",
+        },
+    }
+
+    pod = make_pod_from_dict(d)
+    assert pod.spec.containers[0].name == "dask-0"
+    assert pod.spec.containers[1].name == "sidecar"
+    assert pod.spec.containers[2].name == "dask-2"
