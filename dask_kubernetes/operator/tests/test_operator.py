cd6f508ee16a2da5cf74a597cb623b9aa002ecc6
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-02-01 16:12:09 +0000

    Move operator runner into fixture

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -9,29 +9,32 @@ from kopf.testing import KopfRunner
 DIR = pathlib.Path(__file__).parent.absolute()
 
 
+@pytest.fixture()
+async def operator(k8s_cluster):
+    with KopfRunner(["run", "-m", "dask_kubernetes.operator", "--verbose"]) as runner:
+        yield runner
+
+    # Check operator completed successfully
+    assert runner.exit_code == 0
+    assert runner.exception is None
+
+
 def test_customresources(k8s_cluster):
     assert "daskclusters.kubernetes.dask.org" in k8s_cluster.kubectl("get", "crd")
 
 
 @pytest.mark.timeout(60)
 @pytest.mark.asyncio
-async def test_operator(k8s_cluster):
+async def test_operator(k8s_cluster, operator):
     cluster_path = os.path.join(DIR, "resources", "simplecluster.yaml")
     cluster_name = "simple-cluster"
 
-    # Start operator
-    with KopfRunner(["run", "-m", "dask_kubernetes.operator", "--verbose"]) as runner:
+    # Create cluster resource
+    k8s_cluster.kubectl("apply", "-f", cluster_path)
+    while cluster_name not in k8s_cluster.kubectl("get", "daskclusters"):
+        await asyncio.sleep(1)
 
-        # Create cluster resource
-        k8s_cluster.kubectl("apply", "-f", cluster_path)
-        while cluster_name not in k8s_cluster.kubectl("get", "daskclusters"):
-            await asyncio.sleep(1)
-
-        # Delete cluster resource
-        k8s_cluster.kubectl("delete", "-f", cluster_path)
-        while cluster_name in k8s_cluster.kubectl("get", "daskclusters"):
-            await asyncio.sleep(1)
-
-    # Check operator completed successfully
-    assert runner.exit_code == 0
-    assert runner.exception is None
+    # Delete cluster resource
+    k8s_cluster.kubectl("delete", "-f", cluster_path)
+    while cluster_name in k8s_cluster.kubectl("get", "daskclusters"):
+        await asyncio.sleep(1)
