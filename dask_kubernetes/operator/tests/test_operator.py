a36cd5991c7024da832cc98203a27e582db5067d
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-02-03 15:46:27 +0000

    Refactor fixtures

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -1,6 +1,7 @@
 import pytest
 
 import asyncio
+from contextlib import asynccontextmanager
 import pathlib
 import os.path
 
@@ -10,34 +11,33 @@ DIR = pathlib.Path(__file__).parent.absolute()
 
 
 @pytest.fixture()
-async def operator(k8s_cluster):
-    with KopfRunner(["run", "-m", "dask_kubernetes.operator", "--verbose"]) as runner:
-        yield runner
-
-    # Check operator completed successfully
-    assert runner.exit_code == 0
-    assert runner.exception is None
+async def kopf_runner(k8s_cluster):
+    yield KopfRunner(["run", "-m", "dask_kubernetes.operator", "--verbose"])
 
 
 @pytest.fixture()
-async def simplecluster(k8s_cluster, operator):
-    cluster_path = os.path.join(DIR, "resources", "simplecluster.yaml")
-    cluster_name = "simple-cluster"
+async def gen_cluster(k8s_cluster):
+    """Yields an instantiated context manager for creating/deleting a simple cluster."""
 
-    # Create cluster resource
-    k8s_cluster.kubectl("apply", "-f", cluster_path)
-    while cluster_name not in k8s_cluster.kubectl("get", "daskclusters"):
-        await asyncio.sleep(1)
+    @asynccontextmanager
+    async def cm():
+        cluster_path = os.path.join(DIR, "resources", "simplecluster.yaml")
+        cluster_name = "simple-cluster"
 
-    yield cluster_name
+        # Create cluster resource
+        k8s_cluster.kubectl("apply", "-f", cluster_path)
+        while cluster_name not in k8s_cluster.kubectl("get", "daskclusters"):
+            await asyncio.sleep(0.1)
 
-    # Delete cluster resource
-    k8s_cluster.kubectl("delete", "-f", cluster_path)
-    while cluster_name in k8s_cluster.kubectl("get", "daskclusters"):
-        await asyncio.sleep(1)
+        try:
+            yield cluster_name
+        finally:
+            # Delete cluster resource
+            k8s_cluster.kubectl("delete", "-f", cluster_path)
+            while cluster_name in k8s_cluster.kubectl("get", "daskclusters"):
+                await asyncio.sleep(0.1)
 
-    # FIXME stdout may not have triggered or flushed by this point but we should check that this was successful
-    # assert "A DaskCluster has been created" in operator.stdout
+    yield cm
 
 
 def test_customresources(k8s_cluster):
@@ -45,8 +45,21 @@ def test_customresources(k8s_cluster):
     assert "daskworkergroups.kubernetes.dask.org" in k8s_cluster.kubectl("get", "crd")
 
 
+def test_operator_runs(kopf_runner):
+    with kopf_runner as runner:
+        pass
+
+    assert runner.exit_code == 0
+    assert runner.exception is None
+
+
 @pytest.mark.timeout(60)
 @pytest.mark.asyncio
-async def test_simplecluster(simplecluster):
-    # If we get to this point then all fixtures worked ok and we can actually test some things
-    assert simplecluster
+async def test_simplecluster(kopf_runner, gen_cluster):
+    with kopf_runner as runner:
+        async with gen_cluster() as cluster_name:
+            # TODO test our cluster here
+            assert cluster_name
+
+    assert "A DaskCluster has been created" in runner.stdout
+    # TODO test that the cluster has been cleaned up
