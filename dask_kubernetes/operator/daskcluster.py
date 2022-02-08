208d57f35ad722b5dc60fc6810f3f3548881d0eb
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2022-02-08 09:16:18 -0500

    Create a scheduler pod when DaskCluster resource is created (#397)
    * Create a scheduler pod when DaskCluster resource is created
    
    * Upadate DaskCluster example simple-cluster.yaml
    
    * Add tests for creating scheduler pod and service
    
    * Revert "Add tests for creating scheduler pod and service"
    
    This reverts commit bf58f6ab8fbf735723f2d986d39ea998ad42ed3d.
    
    * Rebase fix merge conflicts
    
    * Check that scheduler pod and service are created
    
    * Fix Dask cluster tests
    
    * Uncomment test
    
    * Kopf is struggling to authenticate in CI, being explicit with config
    
    Co-authored-by: Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
    Co-authored-by: Jacob Tomlinson <jtomlinson@nvidia.com>

--- dask_kubernetes/operator/daskcluster.py
+++ dask_kubernetes/operator/daskcluster.py
@@ -1,8 +1,113 @@
 import kopf
+import kubernetes
+
+
+def build_scheduler_pod_spec(name, image):
+    return {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "name": f"{name}-scheduler",
+            "labels": {
+                "dask.org/cluster-name": name,
+                "dask.org/component": "scheduler",
+            },
+        },
+        "spec": {
+            "containers": [
+                {
+                    "name": "scheduler",
+                    "image": image,
+                    "args": ["dask-scheduler"],
+                    "ports": [
+                        {
+                            "name": "comm",
+                            "containerPort": 8786,
+                            "protocol": "TCP",
+                        },
+                        {
+                            "name": "dashboard",
+                            "containerPort": 8787,
+                            "protocol": "TCP",
+                        },
+                    ],
+                    "readinessProbe": {
+                        "tcpSocket": {"port": "comm"},
+                        "initialDelaySeconds": 5,
+                        "periodSeconds": 10,
+                    },
+                    "livenessProbe": {
+                        "tcpSocket": {"port": "comm"},
+                        "initialDelaySeconds": 15,
+                        "periodSeconds": 20,
+                    },
+                }
+            ]
+        },
+    }
+
+
+def build_scheduler_service_spec(name):
+    return {
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {
+            "name": name,
+            "labels": {
+                "dask.org/cluster-name": name,
+            },
+        },
+        "spec": {
+            "selector": {
+                "dask.org/cluster-name": name,
+                "dask.org/component": "scheduler",
+            },
+            "ports": [
+                {
+                    "name": "comm",
+                    "protocol": "TCP",
+                    "port": 8786,
+                    "targetPort": 8786,
+                },
+                {
+                    "name": "dashboard",
+                    "protocol": "TCP",
+                    "port": 8787,
+                    "targetPort": 8787,
+                },
+            ],
+        },
+    }
 
 
 @kopf.on.create("daskcluster")
-def daskcluster_create(spec, name, namespace, logger, **kwargs):
+async def daskcluster_create(spec, name, namespace, logger, **kwargs):
     logger.info(
         f"A DaskCluster has been created called {name} in {namespace} with the following config: {spec}"
+    )
+    api = kubernetes.client.CoreV1Api()
+
+    # TODO Check for existing scheduler pod
+    data = build_scheduler_pod_spec(name, spec.get("image"))
+    kopf.adopt(data)
+    scheduler_pod = api.create_namespaced_pod(
+        namespace=namespace,
+        body=data,
+    )
+    # await wait_for_scheduler(name, namespace)
+    logger.info(
+        f"A scheduler pod has been created called {data['metadata']['name']} in {namespace} \
+        with the following config: {data['spec']}"
+    )
+
+    # TODO Check for existing scheduler service
+    data = build_scheduler_service_spec(name)
+    kopf.adopt(data)
+    scheduler_pod = api.create_namespaced_service(
+        namespace=namespace,
+        body=data,
+    )
+    logger.info(
+        f"A scheduler service has been created called {data['metadata']['name']} in {namespace} \
+        with the following config: {data['spec']}"
     )
