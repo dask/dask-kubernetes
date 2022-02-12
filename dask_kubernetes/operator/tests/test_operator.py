6b6fc6b63950c4f9d2e470340bc7df5aab03a02d
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-02-11 17:31:26 -0800

    Change endpoint address for daskcluster test

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -75,11 +75,9 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
             # ):
             #     await asyncio.sleep(0.1)
             # await asyncio.sleep(60)
-            with k8s_cluster.port_forward(
-                f"service/{cluster_name}", 8786, localhost=8786
-            ) as port:
+            with k8s_cluster.port_forward(f"service/{cluster_name}", 8786) as port:
                 async with Client(
-                    f"tcp://localhost:{port}", asynchronous=True
+                    f"{cluster_name}:{port}", asynchronous=True
                 ) as client:
                     await client.wait_for_workers(2)
                     # Ensure that inter-worker communication works well
