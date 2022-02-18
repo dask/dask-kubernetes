7268452e2cb94c52679ea3b1c9d68d41c67bcca2
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-02-18 14:44:24 -0800

    Create separate test for scaling

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -55,7 +55,7 @@ def test_operator_runs(kopf_runner):
     assert runner.exception is None
 
 
-@pytest.mark.timeout(150)
+@pytest.mark.timeout(120)
 @pytest.mark.asyncio
 async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
     with kopf_runner as runner:
@@ -77,6 +77,46 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
                 async with Client(
                     f"tcp://localhost:{port}", asynchronous=True
                 ) as client:
+                    # k8s_cluster.kubectl(
+                    #     "scale",
+                    #     "--replicas=5",
+                    #     "daskworkergroup",
+                    #     "default-worker-group",
+                    # )
+                    await client.wait_for_workers(2)
+                    # Ensure that inter-worker communication works well
+                    futures = client.map(lambda x: x + 1, range(10))
+                    total = client.submit(sum, futures)
+                    assert (await total) == sum(map(lambda x: x + 1, range(10)))
+                    # k8s_cluster.kubectl(
+                    #     "scale",
+                    #     "--replicas=3",
+                    #     "daskworkergroup",
+                    #     "default-worker-group",
+                    # )
+                    # await client.wait_for_workers(3)
+            assert cluster_name
+
+    assert "A DaskCluster has been created" in runner.stdout
+    assert "A scheduler pod has been created" in runner.stdout
+    assert "A worker group has been created" in runner.stdout
+    # TODO test that the cluster has been cleaned up
+
+
+# @pytest.mark.timeout(120)
+@pytest.mark.asyncio
+async def test_scalesimplecluster(k8s_cluster, kopf_runner, gen_cluster):
+    with kopf_runner as runner:
+        async with gen_cluster() as cluster_name:
+            scheduler_pod_name = "simple-cluster-scheduler"
+            while "Running" not in k8s_cluster.kubectl(
+                "get", "pods", scheduler_pod_name
+            ):
+                asyncio.wait(0.1)
+            with k8s_cluster.port_forward(f"service/{cluster_name}", 8786) as port:
+                async with Client(
+                    f"tcp://localhost:{port}", asynchronous=True
+                ) as client:
                     k8s_cluster.kubectl(
                         "scale",
                         "--replicas=5",
@@ -84,10 +124,6 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
                         "default-worker-group",
                     )
                     await client.wait_for_workers(5)
-                    # Ensure that inter-worker communication works well
-                    futures = client.map(lambda x: x + 1, range(10))
-                    total = client.submit(sum, futures)
-                    assert (await total) == sum(map(lambda x: x + 1, range(10)))
                     k8s_cluster.kubectl(
                         "scale",
                         "--replicas=3",
@@ -95,9 +131,3 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
                         "default-worker-group",
                     )
                     await client.wait_for_workers(3)
-            assert cluster_name
-
-    assert "A DaskCluster has been created" in runner.stdout
-    assert "A scheduler pod has been created" in runner.stdout
-    assert "A worker group has been created" in runner.stdout
-    # TODO test that the cluster has been cleaned up
