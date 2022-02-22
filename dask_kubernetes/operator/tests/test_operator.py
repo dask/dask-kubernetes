6dd284b0451546a4c818b7c8e72675068c5055f9
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-02-22 09:06:46 -0800

    Test that cluster has been cleaned up

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -97,12 +97,12 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
                     # await client.wait_for_workers(3)
             assert cluster_name
 
-    assert "A DaskCluster has been created" in runner.stdout
-    assert "A scheduler pod has been created" in runner.stdout
-    assert "A worker group has been created" in runner.stdout
     # TODO test that the cluster has been cleaned up
     while "simple-cluster" in k8s_cluster.kubectl("get", "daskclusters"):
         await asyncio.sleep(0.1)
+    assert "A DaskCluster has been created" in runner.stdout
+    assert "A scheduler pod has been created" in runner.stdout
+    assert "A worker group has been created" in runner.stdout
 
 
 # @pytest.mark.timeout(120)
