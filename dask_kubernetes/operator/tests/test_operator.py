3c8e9cb019b1ff1df814fd45349fcefe33402c2f
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-02-22 10:13:29 -0800

    Wait for cluster to be ready

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -116,6 +116,18 @@ async def test_simplecluster(k8s_cluster, kopf_runner, gen_cluster):
 async def test_scalesimplecluster(k8s_cluster, kopf_runner, gen_cluster):
     with kopf_runner as runner:
         async with gen_cluster() as cluster_name:
+            scheduler_pod_name = "simple-cluster-scheduler"
+            worker_pod_name = "default-worker-group-worker-1"
+            while scheduler_pod_name not in k8s_cluster.kubectl("get", "pods"):
+                await asyncio.sleep(0.1)
+            while "Running" not in k8s_cluster.kubectl(
+                "get", "pods", scheduler_pod_name
+            ):
+                await asyncio.sleep(0.1)
+            while cluster_name not in k8s_cluster.kubectl("get", "svc"):
+                await asyncio.sleep(0.1)
+            while worker_pod_name not in k8s_cluster.kubectl("get", "pods"):
+                await asyncio.sleep(0.1)
             with k8s_cluster.port_forward(f"service/{cluster_name}", 8786) as port:
                 async with Client(
                     f"tcp://localhost:{port}", asynchronous=True
