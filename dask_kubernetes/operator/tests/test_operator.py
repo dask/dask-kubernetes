599b5e598d1d99bc2b6a2efe8b8ab1886d7991e8
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-02-21 18:11:26 -0800

    Add sleep to scaling test

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -158,6 +158,7 @@ async def test_scalesimplecluster(k8s_cluster, kopf_runner, gen_cluster):
             while worker_pod_name not in k8s_cluster.kubectl("get", "pods"):
                 await asyncio.sleep(0.1)
 
+            asyncio.sleep(60)
             with k8s_cluster.port_forward(f"service/{cluster_name}", 8786) as port:
                 async with Client(
                     f"tcp://localhost:{port}", asynchronous=True
