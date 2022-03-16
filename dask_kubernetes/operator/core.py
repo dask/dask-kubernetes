343a063d977d5898aeb750c30354a006bf79c504
Matthew Murray <mmurray@dgx15.aselab.nvidia.com>
2022-03-16 12:21:23 -0700

    Connect cluster manager to cluster and add additional worker method

--- dask_kubernetes/operator/core.py
+++ dask_kubernetes/operator/core.py
@@ -1,14 +1,17 @@
 import subprocess
 import json
 import re
+import time
 
+from distributed.core import rpc
 from distributed.deploy import Cluster
 
 from distributed.utils import Log, Logs
 
 from dask_kubernetes.auth import ClusterAuth
-from daskcluster import (
+from .daskcluster import (
     build_cluster_spec,
+    build_worker_group_spec,
 )
 
 
@@ -43,7 +46,6 @@ class KubeCluster2(Cluster):
             self.sync(self._start)
 
     async def _start(self):
-        await ClusterAuth.load_first(self.auth)
         data = build_cluster_spec(
             self.name, self.image, self.replicas, self.resources, self.env
         )
@@ -60,15 +62,33 @@ class KubeCluster2(Cluster):
             ],
             encoding="utf-8",
         )
+        time.sleep(5)
+        pf = subprocess.Popen(
+            [
+                "kubectl",
+                "port-forward",
+                f"service/{self.name}-cluster",
+                "8786:8786",
+                "-n",
+                f"{self.namespace}",
+            ],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            start_new_session=True,
+        )
+        time.sleep(2)
+        self.scheduler_comm = rpc("localhost:8786")
+        await super()._start()
 
     async def _get_scheduler_address(self):
         address = str(
             re.search(
                 r"Scheduler at:   (.*)\\ndistributed.scheduler",
-                self.get_logs()["foo-cluster-scheduler"],
+                self.get_logs()["simple-cluster-scheduler"],
             )
         ).split(" ")[-1][:-11]
-        return address
+        return {address}
 
     def get_logs(self):
         """Get logs for Dask scheduler and workers."""
@@ -104,6 +124,22 @@ class KubeCluster2(Cluster):
             )
             logs[name] = log
         return logs
+
+    def add_worker_group(self, name, n=3):
+        data = build_worker_group_spec(name, self.image, n, self.resources, self.env)
+        with open("data.json", "w") as jfile:
+            json.dump(data, jfile)
+        workers = subprocess.check_output(
+            [
+                "kubectl",
+                "apply",
+                "-f",
+                "data.json",
+                "-n",
+                self.namespace,
+            ],
+            encoding="utf-8",
+        )
 
     def scale(self, worker_group, n):
         scaler = subprocess.check_output(
