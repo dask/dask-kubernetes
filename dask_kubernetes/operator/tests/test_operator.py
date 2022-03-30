189494765a4047685a6f9d76d77ab01be78ddecf
Matt711 <mmurray@nvidia.com>
2022-03-30 12:52:53 -0400

    Remove async from KubeCluster2

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -160,7 +160,7 @@ from dask_kubernetes.operator.core import KubeCluster2
 async def test_scale_kubecluster2(kopf_runner, k8s_cluster, gen_cluster2):
     with kopf_runner as runner:
         async with gen_cluster2() as cluster_name:
-            cluster = KubeCluster2(name=cluster_name, asynchronous=True)
+            cluster = KubeCluster2(name=cluster_name)
             scheduler_pod_name = f"{cluster_name}-cluster-scheduler"
             worker_pod_name = f"{cluster_name}-cluster-default-worker-group-worker"
             while scheduler_pod_name not in k8s_cluster.kubectl("get", "pods"):
