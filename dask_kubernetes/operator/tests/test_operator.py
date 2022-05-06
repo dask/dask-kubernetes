9c11b067399c88b4c2b9903e345e391619dbbf77
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-05-06 12:30:34 +0100

    Implement from name and connecting to existing clusters

--- dask_kubernetes/operator/tests/test_operator.py
+++ dask_kubernetes/operator/tests/test_operator.py
@@ -176,3 +176,10 @@ def test_multiple_clusters_simultaneously(kopf_runner):
             with Client(cluster1) as client1, Client(cluster2) as client2:
                 assert client1.submit(lambda x: x + 1, 10).result() == 11
                 assert client2.submit(lambda x: x + 1, 10).result() == 11
+
+
+def test_cluster_from_name(kopf_runner):
+    with kopf_runner:
+        with KubeCluster(name="bar") as firstcluster:
+            with KubeCluster.from_name("bar") as secondcluster:
+                assert firstcluster == secondcluster
