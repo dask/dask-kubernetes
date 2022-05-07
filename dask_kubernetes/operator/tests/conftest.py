f12c80345f509a8b4a82bcbc7e3406e2ba408623
Sam Dyson <sam.dyson@quantifiedstrategies.com.au>
2022-05-07 11:47:27 +1000

    test: add step to build new CRD inside conftest.py, update test fixtures, and add k8s-crd-resolver to test requirements

--- dask_kubernetes/operator/tests/conftest.py
+++ dask_kubernetes/operator/tests/conftest.py
@@ -1,44 +1,12 @@
 import pytest
 
+from glob import glob
 import pathlib
 import os
-import subprocess
 import tempfile
-
-from kopf.testing import KopfRunner
-
-from dask_kubernetes.common.utils import check_dependency
+import subprocess
 
 DIR = pathlib.Path(__file__).parent.absolute()
-
-check_dependency("helm")
-check_dependency("kubectl")
-check_dependency("docker")
-
-
-@pytest.fixture()
-async def kopf_runner(k8s_cluster):
-    yield KopfRunner(["run", "-m", "dask_kubernetes.operator", "--verbose"])
-
-
-@pytest.fixture(scope="session")
-def docker_image():
-    image_name = "dask-kubernetes:dev"
-    subprocess.check_output(["docker", "build", "-t", image_name, "./ci/"])
-    return image_name
-
-
-@pytest.fixture(scope="session")
-def k8s_cluster(kind_cluster, docker_image):
-    os.environ["KUBECONFIG"] = str(kind_cluster.kubeconfig_path)
-    kind_cluster.load_docker_image(docker_image)
-    yield kind_cluster
-    del os.environ["KUBECONFIG"]
-
-
-@pytest.fixture(scope="session")
-def ns(k8s_cluster):
-    return "default"
 
 
 def run_generate(crd_path, patch_path, temp_path):
