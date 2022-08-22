2c7c7a53450876f0db6aa6b17a62083ff5a5d968
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-08-22 15:11:14 +0100

    Fix trailing space

--- dask_kubernetes/operator/plugins/noop/noop.py
+++ dask_kubernetes/operator/plugins/noop/noop.py
@@ -5,6 +5,6 @@ import kopf
 async def noop_startup(logger, **kwargs):
     logger.info(
         "Plugin 'noop' running. This does nothing. "
-        "See https://kubernetes.dask.org/en/latest/operator_extending.html for details"
-        "on writing plugins for the Dask Operator controller."
+        "See https://kubernetes.dask.org/en/latest/operator_extending.html "
+        "for details on writing plugins for the Dask Operator controller."
     )
