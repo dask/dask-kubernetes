c1731c31a497e4658e52fc80989ee9eae83fca93
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-09-01 14:57:32 +0100

    Make async

--- dask_kubernetes/experimental/tests/test_discovery.py
+++ dask_kubernetes/experimental/tests/test_discovery.py
@@ -9,8 +9,8 @@ from dask_kubernetes.experimental.discovery import discover
 async def test_discovery(cluster):
     clusters = [name async for name, _ in discover()]
     assert cluster.name in clusters
-    with KubeCluster.from_name(cluster.name) as cluster2:
+    async with KubeCluster.from_name(cluster.name, asynchronous=True) as cluster2:
         assert cluster == cluster2
         assert "id" in cluster2.scheduler_info
-        with Client(cluster2) as client:
-            assert client.submit(lambda x: x + 1, 10).result() == 11
+        async with Client(cluster2, asynchronous=True) as client:
+            assert await client.submit(lambda x: x + 1, 10).result() == 11
