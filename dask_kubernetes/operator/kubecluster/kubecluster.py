409926eac015757f51df15f8d3cbd41e071e0802
Bas Welsh <sebastian@ventrs.io>
2022-10-28 06:06:34 -0400

    Update kubecluster.py (#591)

--- dask_kubernetes/operator/kubecluster/kubecluster.py
+++ dask_kubernetes/operator/kubecluster/kubecluster.py
@@ -848,5 +848,4 @@ def reap_clusters():
                     else:
                         cluster.close(timeout=10)
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_reap_clusters())
+    asyncio.run(_reap_clusters())
