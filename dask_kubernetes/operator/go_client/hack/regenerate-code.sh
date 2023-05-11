cef5b40fa7552657bc28578fa25109e602af7dc9
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2023-05-11 20:42:29 +0300

    Update new generation files

---
+++ dask_kubernetes/operator/go_client/hack/regenerate-code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE})
+REPO_DIR=$(realpath "$CURRENT_DIR/../../../..")
+
+PROJECT_MODULE="github.com/dask/dask-kubernetes"
+IMAGE_NAME="kubernetes-codegen:latest"
+
+CUSTOM_RESOURCE_NAME="kubernetes.dask.org"
+CUSTOM_RESOURCE_VERSION="v1"
+
+echo "Building codegen Docker image..."
+docker build -f "${CURRENT_DIR}/Dockerfile" \
+             -t "${IMAGE_NAME}" \
+             .
+
+cmd="./generate-groups.sh all \
+    "$PROJECT_MODULE/dask_kubernetes/operator/go_client/pkg/client" \
+    "$PROJECT_MODULE/dask_kubernetes/operator/go_client/pkg/apis" \
+    $CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
+
+# FIXME
+echo $cmd
+
+# FIXME: Remove -it
+echo "Generating client codes..."
+docker run -it --rm \
+           -v "${REPO_DIR}:/go/src/${PROJECT_MODULE}" \
+           "${IMAGE_NAME}" $cmd
