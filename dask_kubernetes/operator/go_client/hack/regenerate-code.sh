6ab4bd95507e72af7004601ca21adb8ef5ca25bd
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2023-05-11 20:48:32 +0300

    Clean up `regenerate-code.sh`

--- dask_kubernetes/operator/go_client/hack/regenerate-code.sh
+++ dask_kubernetes/operator/go_client/hack/regenerate-code.sh
@@ -19,11 +19,8 @@ cmd="./generate-groups.sh all \
     "$PROJECT_MODULE/dask_kubernetes/operator/go_client/pkg/apis" \
     $CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
 
-# FIXME
-echo $cmd
 
-# FIXME: Remove -it
 echo "Generating client codes..."
-docker run -it --rm \
+docker run --rm \
            -v "${REPO_DIR}:/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" $cmd
