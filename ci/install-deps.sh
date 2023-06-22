9235113c13af1d61fe1ab78f33f8cc9c9e6a7735
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-06-22 10:46:03 +0100

    Pin to commit that I suspect is causing this, expect this run to fail

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -7,5 +7,5 @@ mv istio-*/bin/istioctl /usr/local/bin/istioctl
 
 pip install -e .
 pip install -r requirements-test.txt
-pip install git+https://github.com/dask/distributed@74a1bcd
+pip install git+https://github.com/dask/distributed@e7cb0d80fad01c34ed0418057a062dfb0971e2c8
 pip install git+https://github.com/dask/dask@main
