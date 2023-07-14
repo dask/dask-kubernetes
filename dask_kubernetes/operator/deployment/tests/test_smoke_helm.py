66b07844b60286fb54a94e1e10cb8d6ce7503892
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2023-07-14 14:21:08 +0200

    Add test with `--set=rbac.create=false`

--- dask_kubernetes/operator/deployment/tests/test_smoke_helm.py
+++ dask_kubernetes/operator/deployment/tests/test_smoke_helm.py
@@ -48,8 +48,49 @@ def install_cluster_role_helm_chart(k8s_cluster, operator_image, ns):
     )
 
 
+@pytest.fixture
+def install_role_helm_chart(k8s_cluster, operator_image, ns):
+    this_dir = Path(__file__).parent
+    helm_chart_dir = this_dir.parent / "helm" / "dask-kubernetes-operator"
+    release_name = f"pytest-smoke-operator-{str(uuid.uuid4())[:8]}"
+    docker_image_name = operator_image.split(":")[0]
+    docker_image_tag = operator_image.split(":")[1]
+    subprocess.run(
+        [
+            "helm",
+            "install",
+            "-n",
+            ns,
+            release_name,
+            str(helm_chart_dir),
+            f"--set=image.name={docker_image_name}",
+            f"--set=image.tag={docker_image_tag}",
+            "--set=rbac.create=false",
+            "--wait",
+        ],
+        check=True,
+    )
+    yield
+    subprocess.run(
+        ["helm", "uninstall", "-n", ns, release_name],
+        check=True,
+    )
+
+
 @pytest.mark.timeout(180)
-def test_smoke_helm_deployment_role(install_cluster_role_helm_chart, ns):
+def test_smoke_helm_deployment_cluster_role(install_cluster_role_helm_chart, ns):
+    with KubeCluster(
+        name="pytest-smoke-cluster",
+        namespace=ns,
+        shutdown_on_close=True,
+    ) as cluster:
+        cluster.scale(2)
+        client = cluster.get_client()
+        client.wait_for_workers(2, timeout=120)
+
+
+@pytest.mark.timeout(180)
+def test_smoke_helm_deployment_role(install_role_helm_chart, ns):
     with KubeCluster(
         name="pytest-smoke-cluster",
         namespace=ns,
