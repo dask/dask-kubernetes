1beaf01b7fedbe7d5d63dc14acdceb756c4926f2
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-07-27 10:27:01 +0100

    Replace kubernetes_asyncio with kr8s in cluster discovery

--- dask_kubernetes/operator/kubecluster/discovery.py
+++ dask_kubernetes/operator/kubecluster/discovery.py
@@ -1,26 +1,14 @@
 from typing import Callable, AsyncIterator, Tuple
 
-import kubernetes_asyncio as kubernetes
+import kr8s
 
 from dask_kubernetes.operator.kubecluster import KubeCluster
-from dask_kubernetes.common.auth import ClusterAuth
-from dask_kubernetes.common.utils import get_current_namespace
 
 
 async def discover() -> AsyncIterator[Tuple[str, Callable]]:
-
-    await ClusterAuth.load_first()
-
     try:
-        async with kubernetes.client.api_client.ApiClient() as api_client:
-            custom_objects_api = kubernetes.client.CustomObjectsApi(api_client)
-            clusters = await custom_objects_api.list_namespaced_custom_object(
-                group="kubernetes.dask.org",
-                version="v1",
-                plural="daskclusters",
-                namespace=get_current_namespace(),
-            )
-        for cluster in clusters["items"]:
-            yield (cluster["metadata"]["name"], KubeCluster)
+        clusters = await kr8s.asyncio.get("daskclusters", namespace=kr8s.ALL)
+        for cluster in clusters:
+            yield (cluster.name, KubeCluster)
     except Exception:
         return
