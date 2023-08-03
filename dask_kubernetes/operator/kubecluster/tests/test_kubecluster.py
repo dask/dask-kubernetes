8ac01e47b2c367d7e6046800117b34b33aca84b0
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-08-03 13:10:13 +0100

    Use namespace in all tests

--- dask_kubernetes/operator/kubecluster/tests/test_kubecluster.py
+++ dask_kubernetes/operator/kubecluster/tests/test_kubecluster.py
@@ -26,56 +26,66 @@ def test_kubecluster(cluster):
 
 
 @pytest.mark.anyio
-async def test_kubecluster_async(kopf_runner, docker_image):
+async def test_kubecluster_async(kopf_runner, docker_image, ns):
     with kopf_runner:
         async with KubeCluster(
             name="async",
             image=docker_image,
             n_workers=1,
             asynchronous=True,
+            namespace=ns,
         ) as cluster:
             async with Client(cluster, asynchronous=True) as client:
                 assert await client.submit(lambda x: x + 1, 10).result() == 11
 
 
-def test_custom_worker_command(kopf_runner, docker_image):
+def test_custom_worker_command(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
             name="customworker",
             image=docker_image,
             worker_command=["python", "-m", "distributed.cli.dask_worker"],
             n_workers=1,
+            namespace=ns,
         ) as cluster:
             with Client(cluster) as client:
                 assert client.submit(lambda x: x + 1, 10).result() == 11
 
 
-def test_multiple_clusters(kopf_runner, docker_image):
+def test_multiple_clusters(kopf_runner, docker_image, ns):
     with kopf_runner:
-        with KubeCluster(name="bar", image=docker_image, n_workers=1) as cluster1:
+        with KubeCluster(
+            name="bar", image=docker_image, n_workers=1, namespace=ns
+        ) as cluster1:
             with Client(cluster1) as client1:
                 assert client1.submit(lambda x: x + 1, 10).result() == 11
-        with KubeCluster(name="baz", image=docker_image, n_workers=1) as cluster2:
+        with KubeCluster(
+            name="baz", image=docker_image, n_workers=1, namespace=ns
+        ) as cluster2:
             with Client(cluster2) as client2:
                 assert client2.submit(lambda x: x + 1, 10).result() == 11
 
 
-def test_clusters_with_custom_port_forward(kopf_runner, docker_image):
+def test_clusters_with_custom_port_forward(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
-            name="bar", image=docker_image, n_workers=1, scheduler_forward_port=8888
+            name="bar",
+            image=docker_image,
+            n_workers=1,
+            scheduler_forward_port=8888,
+            namespace=ns,
         ) as cluster1:
             assert cluster1.forwarded_dashboard_port == "8888"
             with Client(cluster1) as client1:
                 assert client1.submit(lambda x: x + 1, 10).result() == 11
 
 
-def test_multiple_clusters_simultaneously(kopf_runner, docker_image):
+def test_multiple_clusters_simultaneously(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
-            name="fizz", image=docker_image, n_workers=1
+            name="fizz", image=docker_image, n_workers=1, namespace=ns
         ) as cluster1, KubeCluster(
-            name="buzz", image=docker_image, n_workers=1
+            name="buzz", image=docker_image, n_workers=1, namespace=ns
         ) as cluster2:
             with Client(cluster1) as client1, Client(cluster2) as client2:
                 assert client1.submit(lambda x: x + 1, 10).result() == 11
@@ -83,12 +93,16 @@ def test_multiple_clusters_simultaneously(kopf_runner, docker_image):
 
 
 @pytest.mark.skip(reason="Flaky and fails ~10% of the time.")
-def test_multiple_clusters_simultaneously_same_loop(kopf_runner, docker_image):
+def test_multiple_clusters_simultaneously_same_loop(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
-            name="fizz", image=docker_image, n_workers=1
+            name="fizz", image=docker_image, n_workers=1, namespace=ns
         ) as cluster1, KubeCluster(
-            name="buzz", image=docker_image, loop=cluster1.loop, n_workers=1
+            name="buzz",
+            image=docker_image,
+            loop=cluster1.loop,
+            n_workers=1,
+            namespace=ns,
         ) as cluster2:
             with Client(cluster1) as client1, Client(cluster2) as client2:
                 assert cluster1.loop is cluster2.loop is client1.loop is client2.loop
@@ -122,10 +136,10 @@ def test_cluster_scheduler_info_updated(kopf_runner, docker_image, ns):
                 assert firstcluster.scheduler_info == secondcluster.scheduler_info
 
 
-def test_additional_worker_groups(kopf_runner, docker_image):
+def test_additional_worker_groups(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
-            name="additionalgroups", n_workers=1, image=docker_image
+            name="additionalgroups", n_workers=1, image=docker_image, namespace=ns
         ) as cluster:
             cluster.add_worker_group(name="more", n_workers=1)
             with Client(cluster) as client:
@@ -134,27 +148,39 @@ def test_additional_worker_groups(kopf_runner, docker_image):
             cluster.delete_worker_group(name="more")
 
 
-def test_cluster_without_operator(docker_image):
+def test_cluster_without_operator(docker_image, ns):
     with pytest.raises(TimeoutError, match="is the Dask Operator running"):
-        KubeCluster(name="noop", n_workers=1, image=docker_image, resource_timeout=1)
+        KubeCluster(
+            name="noop",
+            n_workers=1,
+            image=docker_image,
+            resource_timeout=1,
+            namespace=ns,
+        )
 
 
-def test_cluster_crashloopbackoff(kopf_runner, docker_image):
+def test_cluster_crashloopbackoff(kopf_runner, docker_image, ns):
     with kopf_runner:
         with pytest.raises(SchedulerStartupError, match="Scheduler failed to start"):
             spec = make_cluster_spec(name="crashloopbackoff", n_workers=1)
             spec["spec"]["scheduler"]["spec"]["containers"][0]["args"][
                 0
             ] = "dask-schmeduler"
-            KubeCluster(custom_cluster_spec=spec, resource_timeout=1, idle_timeout=2)
+            KubeCluster(
+                custom_cluster_spec=spec,
+                resource_timeout=1,
+                idle_timeout=2,
+                namespace=ns,
+            )
 
 
-def test_adapt(kopf_runner, docker_image):
+def test_adapt(kopf_runner, docker_image, ns):
     with kopf_runner:
         with KubeCluster(
             name="adaptive",
             image=docker_image,
             n_workers=0,
+            namespace=ns,
         ) as cluster:
             cluster.adapt(minimum=0, maximum=1)
             with Client(cluster) as client:
@@ -166,15 +192,17 @@ def test_adapt(kopf_runner, docker_image):
             cluster.scale(0)
 
 
-def test_custom_spec(kopf_runner, docker_image):
+def test_custom_spec(kopf_runner, docker_image, ns):
     with kopf_runner:
         spec = make_cluster_spec("customspec", image=docker_image)
-        with KubeCluster(custom_cluster_spec=spec, n_workers=1) as cluster:
+        with KubeCluster(
+            custom_cluster_spec=spec, n_workers=1, namespace=ns
+        ) as cluster:
             with Client(cluster) as client:
                 assert client.submit(lambda x: x + 1, 10).result() == 11
 
 
-def test_typo_resource_limits(kopf_runner):
+def test_typo_resource_limits(kopf_runner, ns):
     with kopf_runner:
         with pytest.raises(ValueError):
             KubeCluster(
@@ -184,11 +212,12 @@ def test_typo_resource_limits(kopf_runner):
                         "CPU": "1",
                     },
                 },
+                namespace=ns,
             )
 
 
 @pytest.mark.xfail(reason="Intermittently fails in CI")
-def test_invalid_kwargs_exception(kopf_runner):
+def test_invalid_kwargs_exception(kopf_runner, ns):
     with kopf_runner:
         with pytest.raises(kubernetes.client.ApiException):
-            KubeCluster(name="foo", n_workers="1")
+            KubeCluster(name="foo", n_workers="1", namespace=ns)
