9ff91f4d6bc13bb354208cd6757fa0f7537e547c
Jacob Tomlinson <jtomlinson@nvidia.com>
2024-02-20 11:01:45 +0000

    Upgrade versioneer for python 3.12

--- dask_kubernetes/__init__.py
+++ dask_kubernetes/__init__.py
@@ -2,6 +2,7 @@ from importlib import import_module
 from warnings import warn
 
 from . import config
+from . import _version
 from .common.auth import (
     AutoRefreshConfiguration,
     AutoRefreshKubeConfigLoader,
@@ -14,11 +15,7 @@ from .common.objects import clean_pod_template, make_pod_from_dict, make_pod_spe
 from .helm import HelmCluster
 
 __all__ = ["HelmCluster", "KubeCluster"]
-
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = _version.get_versions()["version"]
 
 
 def __getattr__(name):
