ef41984b54c6507a5a918921da8dfecb24ae4840
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-04-30 09:39:26 +0100

    Migrate to hatch (#889)
    * Migrate to hatch
    
    * Checkout full history
    
    * Install project
    
    * Remove clone depth
    
    * Build package in container multi-stage build to ensure correct versioning
    
    * Remove debugging pip install command
    
    * Fetch full depth to correctly calculate version
    
    * Reinstate entrypoints

--- dask_kubernetes/__init__.py
+++ dask_kubernetes/__init__.py
@@ -2,7 +2,7 @@ from importlib import import_module
 from warnings import warn
 
 from . import config
-from . import _version
+
 from .common.auth import (
     AutoRefreshConfiguration,
     AutoRefreshKubeConfigLoader,
@@ -15,7 +15,13 @@ from .common.objects import clean_pod_template, make_pod_from_dict, make_pod_spe
 from .helm import HelmCluster
 
 __all__ = ["HelmCluster", "KubeCluster"]
-__version__ = _version.get_versions()["version"]
+
+try:
+    from ._version import version as __version__  # noqa
+    from ._version import version_tuple as __version_tuple__  # noqa
+except ImportError:
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
 
 
 def __getattr__(name):
